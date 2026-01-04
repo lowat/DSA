9dcac3dd34edbe202c697ea370094182553c4c69
Logan Watkins <loganwatkins@gmail.com>
2026-01-03 18:58:01 -0600

    Add contains_duplicate.go solution

--- golang/contains_duplicate.go
+++ golang/contains_duplicate.go
@@ -1,5 +1,13 @@
 package main
 
 func ContainsDuplicate(nums []int)bool {
+	seen := make(map[int]bool)
+
+	for _, num := range nums {
+		if _, found := seen[num]; found {
+			return true
+		}
+		seen[num] = true
+	}
 	return false
 }
